12743594e75f6f056f741a0963650677154d4a8b
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-11-20 11:37:08 +0530

    Starting Project

---
+++ backend/controllers/userController.js
@@ -0,0 +1,10 @@
+import asyncHandler from 'express-async-handler'
+
+// @dec      Auth user/set token
+// route     POST /api/users/auth
+// @access   Public
+const authUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: 'Auth User' })
+})
+
+export { authUser }
