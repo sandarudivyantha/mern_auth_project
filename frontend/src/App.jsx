d2be7f7a548b21e65a5b2caa494dce0ff10e3cb0
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-11-24 19:11:06 +0530

    App,Header,Hero,Main,HomeScreen jsx files created

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,16 @@
+import { Container } from 'react-bootstrap';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header';
+
 const App = () => {
   return (
     <>
-      <h1>My App</h1>
+      <Header />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
-  )
+  );
 };
 
 export default App;
