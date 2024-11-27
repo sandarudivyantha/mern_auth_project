e127e9c2bfbea368b799668a4b1f589c74167b98
Sandaru Divyantha <79299187+sandarudivyantha@users.noreply.github.com>
2024-11-27 09:15:35 +0530

    routes updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,12 +9,14 @@ import {
 } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen.jsx';
 
 // Create the router using createBrowserRouter
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index={true} element={<HomeScreen />} />
+      <Route path='/login' element={<LoginScreen />} />
     </Route>
   )
 );
